26cd583b6dbaca4329e40b28fbc42dfd13420a7f
SnzDev <tms.working@gmail.com>
2022-11-09 23:17:17 -0300

    feat: add context fastify with jwt decode

--- api/src/shared/infra/trpc/context.ts
+++ api/src/shared/infra/trpc/context.ts
@@ -1,8 +1,35 @@
+import { IUserRepositories } from '@models/IUserRepositories'
+import { IJWTProvider } from '@shared/providers/JWTProvider/IJWTProvider'
 import { inferAsyncReturnType } from '@trpc/server'
+import { container } from 'tsyringe'
 // import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify'
 
-export async function createContext () {
-  return {}
+export async function createContext ({ req, res }: CreateFastifyContextOptions) {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return { req, res, user: null }
+  }
+
+  const headerParts = authHeader.split(' ')
+
+  if (headerParts.length !== 2) {
+    return { req, res, user: null }
+  }
+
+  const [bearer, token] = headerParts
+
+  if (!/^Bearer$/i.test(bearer)) {
+    return { req, res, user: null }
+  }
+
+  const jwtProvider = container.resolve<IJWTProvider>('JWTProvider')
+  const userRepository = container.resolve<IUserRepositories>('UserRepository')
+  const { id } = await jwtProvider.decodeToken(token)
+
+  const user = await userRepository.findById(id)
+
+  return { req, res, user }
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>
